afcb35905bc968313e1f00f4ac90bfd31f43a6ab
AlexanderYanovych <alexander.yanovych@gmail.com>
2016-09-19 23:36:41 +0300

    added initial framebar styles - temprorary commit

--- src/components/framebar/Framebar.js
+++ src/components/framebar/Framebar.js
@@ -5,7 +5,40 @@ import React, { Component } from 'react';
 class Framebar extends Component {
   render() {
     return (
-      <aside className="framebar"></aside>
+      <aside className="framebar">
+        <div className="framebar__frames-controls">
+          <div className="framebar__frames-controls-button">
+            <svg className="framebar__frames-controls-button__icon" viewBox="0 0 24 24" width="24" height="24">
+              <use xlinkHref="#duplicate"></use>
+            </svg>
+          </div>
+          <div className="framebar__frames-controls-button">
+            <svg className="framebar__frames-controls-button__icon" viewBox="0 0 24 24" width="24" height="24">
+              <use xlinkHref="#remove"></use>
+            </svg>
+          </div>
+          <div className="framebar__frames-controls-button">
+            <svg className="framebar__frames-controls-button__icon" viewBox="0 0 24 24" width="24" height="24">
+              <use xlinkHref="#move-left"></use>
+            </svg>
+          </div>
+          <div className="framebar__frames-controls-button">
+            <svg className="framebar__frames-controls-button__icon" viewBox="0 0 24 24" width="24" height="24">
+              <use xlinkHref="#move-right"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="framebar__container">
+          <div className="framebar__gif-container">
+            <div className="framebar__gif"></div>
+          </div>
+          <div className="framebar__frames-container">
+            <div className="framebar__frame"></div>
+            <div className="framebar__frame"></div>
+            <div className="framebar__frame"></div>
+          </div>
+        </div>
+      </aside>
     )
   }
 }
