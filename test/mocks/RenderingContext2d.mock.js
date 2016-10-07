9342d2c2df0a794f19f2a5dcfd81a8a6c4f81834
Oleksandr Yanovych <oleksandr.yanovych@xyleme.com>
2016-10-07 19:34:17 +0300

    tests for AbstractTool added; RenderingContext2d mock added

---
+++ test/mocks/RenderingContext2d.mock.js
@@ -0,0 +1,28 @@
+import sinon from 'sinon';
+
+const methods = [
+  'fillRect',     'clearRect',
+  'putImageData', 'setTransform',
+  'drawImage',    'save',
+  'fillText',     'restore',
+  'beginPath',    'moveTo',
+  'lineTo',       'closePath',
+  'stroke',       'translate',
+  'scale',        'rotate',
+  'arc',          'fill'
+];
+
+class RenderingContext2d {
+  constructor() {
+    this.canvas = { width: 100, height: 100 };
+  }
+  getImageData(x, y, w, h) { return { data: new Array(w * h * 4) } }
+  createImageData() { return []; }
+};
+
+// adding spies replacing all available CanvasRenderingContext2D methods
+methods.forEach((method) => {
+  RenderingContext2d.prototype[method] = sinon.spy();
+});
+
+export default RenderingContext2d;
--- test/modules/AbstractTool.spec.js
+++ test/modules/AbstractTool.spec.js
@@ -1,6 +1,119 @@
 import test from 'blue-tape'
+import sinon from 'sinon';
+import AbstractTool from 'modules/basetool/AbstractTool';
+import RenderingContext2d from '../mocks/RenderingContext2d.mock';
 
-test('Tests run', (assert) => {
-  assert.pass('Tests run');
-  assert.end();
+let tool, context;
+
+const before = () => {
+  tool = new AbstractTool();
+  context = new RenderingContext2d();
+};
+
+test('AbstractTool =>', (expect) => {
+  expect.test('::_assignBufferContext, ::_assignRenderingContext', (expect) => {
+    before();
+
+    tool.useStateOn = sinon.spy();
+    tool._assignRenderingContext(context);
+    expect.ok(tool._ctx, 'Should assign new rendering context');
+
+    tool._assignBufferContext(context);
+    expect.ok(tool._buffer, 'Should assign new buffer context');
+    expect.ok(context.clearRect.called, 'Should clear the buffer context surface when assigned');
+    expect.ok(tool.useStateOn.called, 'Should apply state on buffer context');
+    expect.end();
+  });
+
+  expect.test('::applyState', (expect) => {
+    before();
+
+    const newState = {
+      ghostData: {
+        color: '#ababab',
+        alpha: 1
+      }
+    };
+
+    tool.applyState(newState);
+    expect.deepEqual(tool.state.ghostData, newState.ghostData, 'Should set new props on the tools state');
+    expect.end();
+  });
+
+  expect.test('::useStateOn', (expect) => {
+    before();
+
+    const state = {
+      size: tool.state.size,
+      color: tool.state.color,
+      alpha: tool.state.alpha,
+      compositeOperation: tool.state.compositeOperation
+    };
+
+    tool.useStateOn(context);
+
+    const {
+      lineWidth: size,
+      fillStyle: color,
+      globalAlpha: alpha,
+      globalCompositeOperation: compositeOperation
+    } = context;
+
+    expect.deepEqual({ size, color, alpha, compositeOperation }, state, 'Should set new state on context');
+    expect.end();
+  });
+
+  expect.test('::useGhostStateOn', (expect) => {
+    before();
+
+    const state = {
+      alpha: tool.state.ghostData.alpha,
+      color: tool.state.ghostData.color
+    };
+
+    tool.useGhostStateOn(context);
+
+    const {
+      globalAlpha: alpha,
+      fillStyle: color
+    } = context;
+
+    expect.deepEqual({ alpha, color }, state, 'Should set new ghost state on context');
+    expect.end();
+  });
+
+  expect.test('::getPixeledCoords', (expect) => {
+    before();
+
+    let coords;
+
+    tool.applyState({ size: 20, gridCellSize: 10 });
+
+    expect.false(tool.getPixeledCoords(), 'Should return false on empty args');
+
+    coords = tool.getPixeledCoords(311, 207);
+
+    expect.deepEqual(coords, { x: 300, y: 190 }, 'Should truncate input coords to grid cell size')
+    expect.end();
+  });
+
+  expect.test('::drawPixelCell', (expect) => {
+    before();
+
+    tool.drawPixelCell(context, 100, 100);
+
+    expect.ok(context.fillRect.called, 'Should draw pixel cell with provided coordinates');
+    expect.end();
+  });
+
+  expect.test('::clearPixelCell', (expect) => {
+    before();
+
+    tool.clearPixelCell(context, 100, 100);
+
+    expect.ok(context.clearRect.called, 'Should clear pixel cell with provided coordinates');
+    expect.end();
+  })
+
+  expect.end();
 });
