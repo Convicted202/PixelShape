cf82a2a7538c8068a57eadc8180c4cc6b8ba1077
AlexanderYanovych <alexander.yanovych@gmail.com>
2017-01-30 15:06:16 +0200

    Added constraints of 600 to input values in Settings modal

--- src/components/modals/Settings.js
+++ src/components/modals/Settings.js
@@ -3,18 +3,93 @@ import ModalWindow from 'components/modalwindow/Modalwindow';
 import ToggleCheckbox from 'components/togglecheckbox/Togglecheckbox';
 import CanvasAnchors from 'containers/canvasanchors/Canvasanchors';
 
+// TODO: take styles from apptoolbox and push in separate related stylesheets
+
+const maxVal = 600,
+      errorColor = '#9a1000',
+      regularColor = '#eee';
+
 class SettingsModal extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+      widthError: false,
+      heightError: false
+    };
+  }
+
+  resetErrors () {
+    this.setState({
+      widthError: false,
+      heightError: false
+    });
+  }
+
+  isInputValid (input) {
+    return input.value <= maxVal;
+  }
+
+  checkErrors () {
+    let ok = false;
+
+    if (!this.isInputValid(this._widthInput)) {
+      this.setState({ widthError: true });
+      ok = true;
+    }
+    if (!this.isInputValid(this._heightInput)) {
+      this.setState({ heightError: true });
+      ok = true;
+    }
+    return ok;
+  }
+
+  getErrorMessage () {
+    if (this.state.widthError || this.state.heightError) {
+      return (
+        <span className="apptoolbox__dimensions-inputs-error">Sorry, max allowed value is 600</span>
+      );
+    }
+    return null;
+  }
+
+  setActualInputValues () {
+    this._widthInput.value = this.props.imageSize.width;
+    this._heightInput.value = this.props.imageSize.height;
   }
 
   confirm () {
+    this.resetErrors();
+    if (this.checkErrors()) return;
     this.props.setImageSize(this._widthInput.value, this._heightInput.value, this.props.stretchOn);
     this.props.closeModal();
   }
 
   cancel () {
+    this.setActualInputValues();
+    this.resetErrors();
     this.props.closeModal();
+  }
+
+  getInputs () {
+    return [
+      <div key="width">
+        <span className="apptoolbox__inputlabel">Width </span>
+        <input
+          className="apptoolbox__inputinline"
+          ref={w => this._widthInput = w}
+          style={{ borderColor: this.state.widthError ? errorColor : regularColor }}
+          defaultValue={this.props.imageSize.width} />
+      </div>,
+
+      <div key="height">
+        <span className="apptoolbox__inputlabel">Height </span>
+        <input
+          className="apptoolbox__inputinline"
+          ref={h => this._heightInput = h}
+          style={{ borderColor: this.state.heightError ? errorColor : regularColor }}
+          defaultValue={this.props.imageSize.width} />
+      </div>
+    ];
   }
 
   render () {
@@ -28,20 +103,8 @@ class SettingsModal extends Component {
         <div className="apptoolbox__dimensions">
           <div className="apptoolbox__dimensions-edit">
             <div className="apptoolbox__dimensions-inputs">
-              <div>
-                <span className="apptoolbox__inputlabel">Width </span>
-                <input
-                  className="apptoolbox__inputinline"
-                  ref={w => this._widthInput = w}
-                  defaultValue={this.props.imageSize.width} />
-              </div>
-              <div>
-                <span className="apptoolbox__inputlabel">Height </span>
-                <input
-                  className="apptoolbox__inputinline"
-                  ref={h => this._heightInput = h}
-                  defaultValue={this.props.imageSize.width} />
-              </div>
+              { this.getInputs() }
+              { this.getErrorMessage() }
             </div>
             <ToggleCheckbox
               className="apptoolbox__dimensions-edit__ratio"
